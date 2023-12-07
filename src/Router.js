68ac7265065e8bf1d37a1cd37f1cbe162a6f4738
DESKTOP-T0RBDHH\82104 <qewryy@naver.com>
2023-12-07 21:12:48 +0900

    쓸모없는 자료 폐기

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
+import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import LoginForm from './LoginForm';
+import LoginForm from './User/LoginForm';
 import Main from './Main';
 
 const Router = () => {
